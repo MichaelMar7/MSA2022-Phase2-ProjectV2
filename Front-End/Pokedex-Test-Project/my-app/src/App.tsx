20f201c1d06fc15c7bf35ec65e7850b2f8c00ca3
MichaelMar7 <michaelmar719@gmail.com>
2022-07-31 15:12:19 +1200

    Pokedex API Step 3

--- Front-End/Pokedex-Test-Project/my-app/src/App.tsx
+++ Front-End/Pokedex-Test-Project/my-app/src/App.tsx
@@ -1,26 +1,38 @@
+import axios from "axios";
 import { useState } from 'react';
 import './App.css';
 
 function App() {
   const [pokemonName, setPokemonName] = useState("");
+  const POKEMON_BASE_URL = "https://pokeapi.co/api/v2";
+
   return (
     <div>
       <h1> Pokemon Search </h1>
+
       <div>
         <label>Pokemon Name</label><br />
-        <input type="text" id="pokemon-name" name="pokemon-name" onChange={e => setPokemonName(e.target.value)} /><br />
-        <button onClick={search}>
-          Search
-        </button>
+        <br />
+        <input 
+          type="text" 
+          id="pokemon-name" 
+          name="pokemon-name" 
+          onChange={e => setPokemonName(e.target.value)}
+        />
+        <br />
+        <button onClick={search}> Search </button>
       </div>
-      <p>
-        You have entered {pokemonName}
-      </p>
+
+      <p> You have entered {pokemonName} </p>
+
+      <div id="pokemon-result">This will show the result</div>
     </div>
   );
 
   function search(){
-    alert("Search button has been clicked!");
+    axios.get(POKEMON_BASE_URL + "/pokemon/" + pokemonName).then((res) => {
+      console.log(res.data);
+    });
   }
 }
 
