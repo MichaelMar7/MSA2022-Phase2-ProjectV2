3cc2bfff33fabeb8db0eebfc21f9ff018b4f60c3
MichaelMar7 <michaelmar719@gmail.com>
2022-08-06 15:02:02 +1200

    csv read and dataframe created

--- Data-Science/Data-Science-Project.py
+++ Data-Science/Data-Science-Project.py
@@ -1,5 +1,18 @@
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+import csv
 
+def part1_EDA():
+    #rawData = part1_EDA_Data()
+    df = pd.read_csv("MSA2022-Phase2-ProjectV2\Data-Science\weather-data.csv")
+    #df.replace("", None)
+    df2 = df["tmpc"].mean()
+    print(df)
+    print(df2)
 
+def main():
+    part1_EDA()
+
+if __name__ == '__main__':
+    main()
