294b64ecb7fa18fb294688a4a0a0bd7e07309d58
MichaelMar7 <michaelmar719@gmail.com>
2022-08-06 21:08:22 +1200

    Part 2 done

--- Data-Science/Data-Science-Project.py
+++ Data-Science/Data-Science-Project.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 from sklearn import datasets
 import numpy as np
 
+from sklearn.model_selection import train_test_split
+
 def part1():
     data = pd.read_csv("MSA2022-Phase2-ProjectV2\Data-Science\weather-data.csv")
     print(data.head())
@@ -42,19 +44,29 @@ def part1():
     plt.show()
     return data
 
-def part1_quick():
+def part1_q():
     data = pd.read_csv("MSA2022-Phase2-ProjectV2\Data-Science\weather-data.csv")
     return data
 
-def part2(data):
-    iris = datasets.load_iris()
-    x = iris.data
-    y = iris.target
-    print(x,y)
+def part2(data):    
+    data = data.drop(data.columns[[0, 6, 7]], axis=1)
+    train, test = train_test_split(data, test_size=0.2, random_state=0)
+    train = train.sort_values("valid")
+    test = test.sort_values("valid")
+    print(train.head())
+    print()
+    print(test.head())
+    print()
+    return train, test
+
+def part2_q(data):
+    data = data.drop(data.columns[[0, 6, 7]], axis=1)
+    train, test = train_test_split(data, test_size=0.2, random_state=0)
+    return train.sort_values("valid"), test.sort_values("valid")
 
 def main():
-    data = part1_quick()
-    part2(data)
+    data = part1_q()
+    train, test = part2_q(data)
 
 if __name__ == '__main__':
     main()
